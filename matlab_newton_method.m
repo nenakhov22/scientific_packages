eca20eb89930d89211600f3d3c3840932348894e
vanes <73476639+og-byte@users.noreply.github.com>
2022-06-06 02:25:02 +0300

    Update and rename matlabb.py to matlab_newton_method.m

---
+++ matlab_newton_method.m
@@ -0,0 +1,29 @@
+clear
+a=0;
+b=14;
+x = linspace(a,b,500);
+h = 0.001; iter = 10000; eps = 0.001;
+%f = @(x)1+(1+sin(x)-cos(x)).^2-(sin(2*x)-cos(2*x)-0.2).^2;
+f = @(x)sin(x)./x;
+y = f(x);
+plot(x,f(x),x,0*x,':');
+grid on
+xlabel('x'); ylabel('y')
+hold on
+ymin=min(y); ymax=max(y);
+if ymin<0 ymin=1.1*ymin; else ymin = 0.9*ymin; end;
+if ymax>0 ymax=1.1*ymax; else ymax = 0.9*ymax; end;
+ylim([ymin,ymax]);
+z = ginput(1);
+x1=z(1);
+for i = 1:iter
+    yh=(f(x1+h)-f(x1))/h;
+    x2=x1-f(x1)/yh;
+    L=line([x2,x2],[0,f(x2)]);
+    set(L,'LineStyle',':')
+    x1=x2;
+    delete(L)
+    if abs(f(x2))<eps break; end;
+end;
+P=plot(x,f(x1)+yh*(x-x1),':',x1,f(x1),'*',x2,0,'*',x2,f(x2),'o')
+hold off
